0ea3ede633ed466d8a4771dbeb9927f4d7b489e5
Mridul Kumar <kumar.mridul000@gmail.com>
2020-10-11 15:50:22 +0530

    Home View

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,46 @@
 <template>
   <div class="home">
-    <h1 class="text-center text-h2">Hello World! Coaching Platform</h1>
+    <HeroHeader />
+    <div class="my-6 my-md-12"></div>
+    <Features />
+    <div class="my-6 my-md-12 py-1"></div>
+    <Accomplishments />
+    <div class="my-6 my-md-12"></div>
+    <Testimonials />
+    <div class="my-6 my-md-12 py-md-2"></div>
+    <ContactUs />
+    <div class="my-6 my-md-12 py-md-6"></div>
+    <Footer />
   </div>
 </template>
 
 <script>
+import HeroHeader from "@/components/Home/HeroHeader.vue";
+import Features from "@/components/Home/Features.vue";
+import Accomplishments from "@/components/Home/Accomplishments.vue";
+import Testimonials from "@/components/Home/Testimonials.vue";
+import ContactUs from "@/components/Home/ContactUs.vue";
+import Footer from "@/components/Home/Footer.vue";
+
 export default {
   name: "Home",
+  metaInfo: {
+    title: "Home",
+    meta: [
+      {
+        vmid: "description",
+        name: "description",
+        content: "Home Page description lorem ipsum dolor sit amet.",
+      },
+    ],
+  },
+  components: {
+    HeroHeader,
+    Features,
+    Accomplishments,
+    Testimonials,
+    ContactUs,
+    Footer,
+  },
 };
 </script>
