1690d208055e2338a883678eeb22184f232d1546
Mridul Kumar <kumar.mridul000@gmail.com>
2020-10-18 14:49:54 +0530

    Implements Admin Home Component w/ ContentRow component and respective assets

---
+++ Admin/src/components/Home/Home/ContentRow.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-row id="contentRow">
+    <!-- Heading Column -->
+    <v-col cols="12" sm="12">
+      <span class="mx-4 px-2 text-h5 white">
+        <v-icon :color="content.colors[0]" class="mr-1">
+          {{ content.icon }}
+        </v-icon>
+        {{ content.title }}
+      </span>
+      <v-divider class="mt-n4"></v-divider>
+    </v-col>
+
+    <!-- Cards Cotainer -->
+    <v-container class="mt-1 mt-md-4">
+      <v-row justify="start">
+        <v-col
+          cols="12"
+          sm="6"
+          md="4"
+          v-for="(option, i) in content.options"
+          :key="i"
+        >
+          <v-card
+            :to="option.to"
+            :color="content.colors[1]"
+            class="mx-auto pt-4"
+            max-width="310"
+          >
+            <v-img
+              height="180px"
+              :src="require(`../../../assets/svg/home/${option.image}`)"
+              contain
+            ></v-img>
+            <div :class="'mt-3 px-1 white--text ' + content.colors[2]">
+              <v-card-title> {{ option.title }} </v-card-title>
+              <v-card-subtitle class="white--text">
+                {{ option.subTitle }}
+              </v-card-subtitle>
+            </div>
+          </v-card>
+        </v-col>
+
+        <!-- Coming Soon Card -->
+        <v-col cols="12" sm="6" md="4">
+          <v-card
+            :color="content.colors[1]"
+            class="mx-auto pt-4"
+            max-width="310"
+          >
+            <v-img
+              height="180px"
+              src="@/assets/svg/home/undraw_loading_frh4.svg"
+              contain
+            ></v-img>
+            <div :class="'mt-3 px-1 white--text ' + content.colors[2]">
+              <v-card-title> Coming Soon </v-card-title>
+              <v-card-subtitle class="white--text">
+                Stay tuned for more features :)
+              </v-card-subtitle>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "ContentRow",
+  props: ["content"],
+};
+</script>
--- Admin/src/views/home/Home.vue
+++ Admin/src/views/home/Home.vue
@@ -5,15 +5,19 @@
       <ProfileAlertRow />
     </v-container>
     <v-container v-else>
-      <span class="text-h3">
-        Profile Complete display admin dash options here</span
-      >
+      <ContentRow :content="options.create" />
+      <div class="my-6 my-md-12"></div>
+      <ContentRow :content="options.manage" />
+      <div class="my-6 my-md-12"></div>
+      <ContentRow :content="options.view" />
+      <div class="my-6 my-md-12"></div>
     </v-container>
   </div>
 </template>
 
 <script>
 import ProfileAlertRow from "@/components/Home/Home/ProfileAlertRow.vue";
+import ContentRow from "@/components/Home/Home/ContentRow.vue";
 
 export default {
   name: "Home",
@@ -29,10 +33,72 @@ export default {
   },
   components: {
     ProfileAlertRow,
+    ContentRow,
   },
   data: () => ({
     loading: true,
     userData: {},
+    options: {
+      create: {
+        title: "Create",
+        icon: "mdi-plus-box-multiple",
+        colors: ["blue darken-4", "blue darken-1", "blue darken-2"],
+        options: [
+          {
+            title: "New Question",
+            subTitle: "Add to the question bank.",
+            to: "/create/question",
+            image: "undraw_publish_post_vowb.svg",
+          },
+          {
+            title: "New Test",
+            subTitle: "Create and deploy a new test.",
+            to: "/create/test",
+            image: "undraw_File_bundle_xl7g.svg",
+          },
+        ],
+      },
+      manage: {
+        title: "Manage",
+        icon: "mdi-playlist-edit",
+        colors: ["pink darken-1", "pink lighten-1", "pink darken-1"],
+        options: [
+          {
+            title: "Edit Tests",
+            subTitle:
+              "Edit existing tests (add/remove questions, visibility, timing etc.)",
+            to: "/manage/edit-tests",
+            image: "undraw_profile_details_f8b7.svg",
+          },
+          {
+            title: "Question Bank",
+            subTitle:
+              "Edit and categorise existing questions (folders, solution etc.)",
+            to: "/manage/question-bank",
+            image: "undraw_collecting_fjjl.svg",
+          },
+        ],
+      },
+      view: {
+        title: "View",
+        icon: "mdi-cards",
+        colors: ["green darken-1", "green lighten-1", "green darken-1"],
+        options: [
+          {
+            title: "Results",
+            subTitle: "Test Results with ranking and score data.",
+            to: "/view/results",
+            image: "undraw_detailed_analysis_xn7y.svg",
+          },
+          {
+            title: "Student Info",
+            subTitle: "Contact information, subscription etc.",
+            to: "/view/student-info",
+            image: "undraw_personal_info_0okl.svg",
+          },
+        ],
+      },
+    },
   }),
   mounted() {
     this.userData = this.$store.getters.userData;
